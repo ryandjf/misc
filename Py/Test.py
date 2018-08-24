ce1bec0eb88ac927bfc5d4b3a40fa3138898e85c
Junfeng Dai <ryandjf@hotmail.com>
2018-08-24 14:10:20 +0800

    add more scripts

--- Py/Test.py
+++ Py/Test.py
@@ -5,7 +5,7 @@ import shutil
 ignoreFiles = ["desktop.ini", ".lock", ".DS_Store"]
 ignoreDestPaths = ["/Users/jfdai/Dropbox/.dropbox.cache", "/Users/jfdai/Dropbox/Downloads", "/Users/jfdai/Dropbox/eBooks", 
     "/Users/jfdai/Dropbox/Music", "/Users/jfdai/Dropbox/Photos", "/Users/jfdai/Dropbox/Samples", "/Users/jfdai/Dropbox/Videos",
-    "/Users/jfdai/Google Drive/Music", "/Users/jfdai/Google Drive/Videos", "/Users/jfdai/Google Drive/Samples", "/Users/jfdai/Google Drive/Downloads",
+    "/Users/jfdai/Google Drive/Music", "/Users/jfdai/Google Drive/Videos", "/Users/jfdai/Google Drive/Samples", "/Users/jfdai/Google Drive/Downloads", "/Users/jfdai/Google Drive/Photos",
     "/Users/jfdai/Box Sync/Downloads", "/Users/jfdai/Box Sync/eBooks", "/Users/jfdai/Box Sync/Music",
     "/Users/jfdai/Box Sync/Photos", "/Users/jfdai/Box Sync/Samples", "/Users/jfdai/Box Sync/Videos"]
 ignoreSrcPaths = []
